1fdc48479a9026da89e1519e8309c2fbb8507854
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2024-05-11 18:13:08 +0300

    release

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-# Номер посылки 113735620
+# Номер посылки 113860882
 import sys
-from array import array
 
 
-def sort_weight(robots_array: array) -> array:
-    last_index: int = len(robots_array) - 1
-    swapped: bool = True
-    while swapped:
-        swapped = False
-        for index in range(last_index):
-            if robots_array[index] > robots_array[index + 1]:
-                robots_array[index], robots_array[index + 1] = (
-                    robots_array[index + 1], robots_array[index]
-                )
-                swapped = True
-        last_index -= 1
-
-    return robots_array
-
-
-def distribute_by_platforms(robots_array: array, limit: int) -> int:
+def distribute_by_platforms(robots_array, limit: int) -> int:
     count: int = 0
     left: int = 0
     right: int = len(robots_array) - 1
@@ -29,20 +12,14 @@ def distribute_by_platforms(robots_array: array, limit: int) -> int:
             right -= 1
             count += 1
             continue
-        else:
-            right -= 1
+        right -= 1
         count += 1
     return count
 
 
-def main() -> None:
+if __name__ == '__main__':
     robots_input_list: list = sys.stdin.readline().rstrip().split()
     limit: int = int(sys.stdin.readline().rstrip())
     robots_input_list = [int(item) for item in robots_input_list]
-    robots_array: array = array('b', robots_input_list)
-    robots_array = sort_weight(robots_array)
+    robots_array: list = sorted(robots_input_list)
     print(distribute_by_platforms(robots_array, limit))
-
-
-if __name__ == '__main__':
-    main()
